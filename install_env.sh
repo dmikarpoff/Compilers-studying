f26a4cff80b89e7fd0352aec811b831ca2b4176e
dmikarpoff <krissoul.sah@gmail.com>
2014-06-12 04:02:47 +0400

    added required packages installation on Ubuntu

--- install_env.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-cd third-party/graphviz-2.38.0
-./configure
-make
-make install
-cd ../..
-
---
+++ install_packages.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+apt-get install graphviz graphviz-dev
+apt-get install flex libfl-dev
+apt-get install bison libbison-dev 
+
